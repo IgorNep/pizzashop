fdb22344cce78554500096e33094f080fb565f33
IGOR <igornesan@gmail.com>
2020-12-18 18:55:37 +0200

    fixed average amount in cart

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -6,6 +6,9 @@ const CartScreen = () => {
   const cartItems = useSelector((state) => state.cartItems);
   const { items, loading, error } = cartItems;
 
+  const placeOrderHandler = () => {
+    alert('Order Placed');
+  };
   return (
     <>
       {loading ? (
@@ -15,39 +18,55 @@ const CartScreen = () => {
       ) : items.length === 0 ? (
         <h2>Cart Is Empty</h2>
       ) : (
-        <table className="cart-table">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Quantity</th>
-              <th>Price</th>
-              <th>Total</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item) => (
-              <tr key={item._id}>
-                <td>{item.name}</td>
-                <td>{item.qty}</td>
-                <td>{item.price} UAH</td>
-                <td>{item.qty * item.price} UAH</td>
+        <div
+          style={{
+            width: '600px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            alignItems: 'flex-end',
+          }}
+        >
+          <table className="cart-table">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item._id}>
+                  <td>{item.name}</td>
+                  <td>{item.qty}</td>
+                  <td>{item.price} UAH</td>
+                  <td>{item.qty * item.price} UAH</td>
+                  <td>
+                    <i
+                      className="fa fa-times"
+                      style={{ color: 'red', cursor: 'pointer' }}
+                    ></i>
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <td></td>
+                <td></td>
+                <td>Average Amount: </td>
                 <td>
-                  <i
-                    className="fa fa-times"
-                    style={{ color: 'red', cursor: 'pointer' }}
-                  ></i>
+                  {items.reduce((acc, item) => item.qty * item.price + acc, 0)}{' '}
+                  UAH
                 </td>
               </tr>
-            ))}
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Average Amount: </td>
-              <td>222.00 uah</td>
-            </tr>
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+          <button className="btn btn-dark" onClick={placeOrderHandler}>
+            Place Order
+          </button>
+        </div>
       )}
     </>
   );
