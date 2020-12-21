3e7bf95b9dfc384e1af569f604fa7ae8d0bb7c1d
IGOR <igornesan@gmail.com>
2020-12-21 13:26:36 +0200

    add user update route and end points

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -7,10 +7,14 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/usersController.js';
 
 router.route('/').get(getUsers).post(registerUser);
 router.route('/login').post(authUser);
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.route('/:id').get(getUserById);
 export default router;
